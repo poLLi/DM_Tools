b54106155d0876feb760135a946eca072d5b6172
PoLLi <1657076+poLLi@users.noreply.github.com>
2020-08-14 13:55:43 +0200

    :sparkles: added Footer Component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
     <div>
         <app-header />
         <Nuxt />
+        <app-footer />
     </div>
 </template>
 
 <script>
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default {
     components: {
-        appHeader: Header
+        appHeader: Header,
+        appFooter: Footer
     }
 };
 </script>
