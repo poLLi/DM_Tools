622d49ddb552f904b50a3d77eb62259bdcbd6a4f
PoLLi <1657076+poLLi@users.noreply.github.com>
2020-08-14 12:54:15 +0200

    :art: changed Index Page
    :camera_flash: added Background Image
    :camera_flash: added Dead Matter Logo Image

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,36 @@
 <template>
-    <div class="container">
-        <div>
-            <Logo />
-            <h1 class="title">
-                JLN | Dead Matter Tools
-            </h1>
-            <div class="links">
-                <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer" class="button--green">
-                    Documentation
-                </a>
-                <a
-                    href="https://github.com/nuxt/nuxt.js"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="button--grey"
-                >
-                    GitHub
-                </a>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, sapiente.</p>
-            </div>
-        </div>
+    <div>
+        <header class="mainhead">
+            <b-container class="h-100">
+                <b-row class="h-100 text-center" align-h="center" align-content="center">
+                    <b-col lg="10">
+                        <img src="~/assets/images/dm_logo.png" alt="Dead Matter Tools" class="mainhead-logo" />
+                        <hr />
+                        <h2 class="text-uppercase text-white font-weight-medium">Web Tools</h2>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </header>
     </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="scss" scoped>
+.mainhead {
+    background: linear-gradient(to bottom, rgba(25, 25, 25, 0.5) 20%, #222831 80%),
+        url(~assets/images/background-highres.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: scroll;
+    background-size: cover;
+    min-height: 40rem;
+    height: 100vh;
+}
+
+.mainhead-logo {
+    width: 450px;
+}
+</style>
