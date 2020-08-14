452d2b1db875ac449dcb0948e5065e7a4a9c80bd
PoLLi <1657076+poLLi@users.noreply.github.com>
2020-08-14 11:39:37 +0200

    :sparkles: added new Header

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-navbar id="mainNav" toggleable="lg" fixed="top" type="dark" variant="dark">
+        <b-container>
+            <b-navbar-brand to="/">
+                JLN | DM Tools
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item to="/" exact>Start</b-nav-item>
+                    <b-nav-item to="/build">Charcater-Builder</b-nav-item>
+                    <b-nav-item to="/map" disabled>Map</b-nav-item>
+                    <b-nav-item-dropdown text="Info" right disabled>
+                        <b-dropdown-item to="/info/weapons" disabled>Weapons</b-dropdown-item>
+                        <b-dropdown-item to="/info/items" disabled>Items</b-dropdown-item>
+                        <b-dropdown-item to="/info/Perks" disabled>Perks</b-dropdown-item>
+                        <b-dropdown-item to="/info/stats" disabled>Stats</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-container>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+};
+</script>
