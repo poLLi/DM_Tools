15dad0975c3f2af0c09d8b9b0faea3b13e92374c
PoLLi <1657076+poLLi@users.noreply.github.com>
2020-08-18 15:58:38 +0200

    Implemented Just-Analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,11 @@ export default {
             { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
             { rel: 'icon', type: 'image/png', href: '/favicon/android-icon-192x192.png', sizes: '192x192' },
             { rel: 'apple-touch-icon', href: '/favicon/apple-icon-180x180.png', sizes: '180x180' }
+        ],
+        script: [
+            {
+                src: '/just-analytics.js'
+            }
         ]
     },
     loading: false,
