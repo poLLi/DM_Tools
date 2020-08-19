639c40d7d840ee6d5854e2f98ab41d9774aae84e
PoLLi <1657076+poLLi@users.noreply.github.com>
2020-08-19 06:42:12 +0200

    SEO improvements
    added a bunch of meta tags
    added localization seo generation
    added extraction of css files on build

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,15 +17,43 @@ export default {
         htmlAttrs: {
             lang: 'en'
         },
-        title: 'JLN | Dead Matter Tools (unofficial)',
+        title: 'Dead Matter Tools — JLN',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'author', content: 'Just-Look Network' },
             {
                 hid: 'description',
                 name: 'description',
                 content:
-                    'Small usefull community build tools for the upcoming Zombie Survival Game Dead Matter from Qi Software, unofficial'
+                    'Unofficial Dead Matter Tools. Everything you need to survive the Zombie Apocalypse in Dead Matter. Use our Character Builder, Ineractive Live Map or our huge database of all the things Dead Matter has to offer.'
+            },
+            {
+                name: 'keywords',
+                content:
+                    'dead matter tools, community web tools, interactive map, database, character builder, open world, zombie, survival, game'
+            },
+
+            // Twitter Meta Tags
+            { name: 'twitter:card', content: 'summary' },
+            { name: 'twitter:site', content: '@JL_PoLLi' },
+            { name: 'twitter:image', content: 'https://dm.just-look.net/dm_tools.png' },
+            { name: 'twitter:title', content: 'Dead Matter Tools — JLN' },
+            {
+                name: 'twitter:description',
+                content:
+                    'Unofficial Dead Matter Tools. Everything you need to survive the Zombie Apocalypse in Dead Matter. Use our Character Builder, Ineractive Live Map or our huge database of all the things Dead Matter has to offer.'
+            },
+
+            // Open Graph Meta Tags
+            { property: 'og:title', content: 'Dead Matter Tools — JLN' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:url', content: 'https://dm.just-look.net' },
+            { property: 'og:image', content: 'https://dm.just-look.net/dm_tools.png' },
+            {
+                property: 'og:description',
+                content:
+                    'Unofficial Dead Matter Tools. Everything you need to survive the Zombie Apocalypse in Dead Matter. Use our Character Builder, Ineractive Live Map or our huge database of all the things Dead Matter has to offer.'
             }
         ],
         link: [
@@ -77,16 +105,19 @@ export default {
             {
                 name: 'English',
                 code: 'en',
+                iso: 'en-US',
                 file: 'en-US.js'
             },
             {
                 name: 'German',
                 code: 'de',
+                iso: 'de-DE',
                 file: 'de-DE.js'
             },
             {
                 name: 'Turkish',
                 code: 'tr',
+                iso: 'tr-TR',
                 file: 'tr-TR.js'
             }
         ],
@@ -111,5 +142,7 @@ export default {
      ** Build configuration
      ** See https://nuxtjs.org/api/configuration-build/
      */
-    build: {}
+    build: {
+        extractCSS: true
+    }
 };
